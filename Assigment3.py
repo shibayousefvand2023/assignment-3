3a406fbe4bd29320b90a3adf61ee39fa14d26b6d
shibayousefvand2023 <140349167+shibayousefvand2023@users.noreply.github.com>
2023-07-31 18:46:36 +0330

    Add files via upload

---
+++ Assigment3.py
@@ -0,0 +1,50 @@
+from ast import While
+import numbers
+from pickle import TRUE
+import random
+from re import S
+from winsound import PlaySound
+
+
+print('Welcome to my dice app')
+
+num1 = repr
+num2 = exit
+num6 = S 
+While-True ;
+if numbers == input('num1 , num2 , num6'):
+    num = random.radiant(1,6)
+    if num == 6:
+        print('S')
+    else:
+        ('break')
+if numbers == 'num2':
+    ('exit') 
+else:
+     print('repr')
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+ 
